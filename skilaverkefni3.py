6183feb1cf47b3da659ddf85da231d0c4244059a
Kalliarolunni <gustigaur@gmail.com>
2019-02-08 13:48:35 +0000

    ya kveen

--- skilaverkefni3.py
+++ skilaverkefni3.py
@@ -12,10 +12,19 @@ def hanoi(n,a,b,c):
 hanoi(3,"a","b","c")
 
 #2
+#flækju stigið er (2^3)-1 3 er þá n-ið
 
-#3
+'''
+3.
+    A) O(n) er lykkja sem keyrir jafn oft og gildið á n
+    B) O(n^2) er lykkja sem er innan í lykkju
+    C) O(log(n)) er 
+
+'''
 
 #4
+#4 A er bullshæt
+'''
 staerd=2
 def abc(n):
     listi=string.ascii_lowercase
@@ -28,14 +37,31 @@ def abc(n):
 
 abc(3)
 '''
-3.
-    A) O(n) er lykkja sem keyrir jafn oft og gildið á n
-    B) O(n^2) er lykkja sem er innan í lykkju
-    C) O(log(n)) er 
+t=string.ascii_lowercase
+def e(t,l,h):
+    p=t[h]
+    i=l-1
+    for x in range(l,h):
+        if t[x]<=p:
+            i+=1
+            tmp=t[x]
+            t[x]=t[i]
+            t[i]=tmp
+    tmp=t[h]
+    t[h]=t[i+1]
+    t[i+1]=tmp
+    return i+1
+
+def q(t,l,h):
+    if l<h:
+        o=e(t,l,h)
+        q(t,l,o-1)
+        q(t,o+1,h)
+
+q(t,0,len(t)-1)
+print(t)
 
 '''
-#flækju stig (2^n)-1
-
 #5
 lis=[]
 def rada(n):
@@ -44,3 +70,29 @@ def rada(n):
 
 
 print(rada(lis))
+'''
+
+#QuickSort
+t=[5,2,7,8,4]
+def e(t,l,h):
+    p=t[h]
+    i=l-1
+    for x in range(l,h):
+        if t[x]<=p:
+            i+=1
+            tmp=t[x]
+            t[x]=t[i]
+            t[i]=tmp
+    tmp=t[h]
+    t[h]=t[i+1]
+    t[i+1]=tmp
+    return i+1
+
+def q(t,l,h):
+    if l<h:
+        o=e(t,l,h)
+        q(t,l,o-1)
+        q(t,o+1,h)
+
+q(t,0,len(t)-1)
+print(t)
