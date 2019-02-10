4610c4adaf93248ba62e774c85be95fe75fca760
Kalliarolunni <gustigaur@gmail.com>
2019-02-10 21:28:16 +0000

    buinn

--- skilaverkefni3.py
+++ skilaverkefni3.py
@@ -12,87 +12,56 @@ def hanoi(n,a,b,c):
 hanoi(3,"a","b","c")
 
 #2
-#flækju stigið er (2^3)-1 3 er þá n-ið
+#flækju stigið er O(2^3)-1 3 er þá n-ið
 
 '''
 3.
     A) O(n) er lykkja sem keyrir jafn oft og gildið á n
     B) O(n^2) er lykkja sem er innan í lykkju
-    C) O(log(n)) er 
+    C) O(log(n)) virkar þannig að t.d. þú ert með bunka af blöðum með nöfnum á en þau eru ekki í stafrófsröð þá tekur það fyrsta nafnið á öllum blaðsíðunum og reður blaðsíðunum eftir þeim
 
 '''
 
 #4
-#4 A er bullshæt
 '''
-staerd=2
-def abc(n):
-    listi=string.ascii_lowercase
-    for x in range(len(listi)):
-        for r in range(len(listi)):
-            if x<r:
-                for i in range(len(listi)):
-                    if r<i:
-                        print(listi[x],listi[r],listi[i])
+a)
+    26!
+__________
+(26-n)!*n!
 
-abc(3)
+b)
+O(2^n)
 '''
 t=string.ascii_lowercase
-def e(t,l,h):
-    p=t[h]
-    i=l-1
-    for x in range(l,h):
-        if t[x]<=p:
-            i+=1
-            tmp=t[x]
-            t[x]=t[i]
-            t[i]=tmp
-    tmp=t[h]
-    t[h]=t[i+1]
-    t[i+1]=tmp
-    return i+1
-
-def q(t,l,h):
-    if l<h:
-        o=e(t,l,h)
-        q(t,l,o-1)
-        q(t,o+1,h)
-
-q(t,0,len(t)-1)
-print(t)
-
+y =[]
+def q(n,s="",r = 0):
+    if n > 0:
+        for x in range(r,len(t)):
+            if n == 1:
+                y.append(s+t[x])
+            r+=1
+            q(n-1,s+t[x],r)
+            
+lengd = int(input("Veldu lengd strengsins: "))
+q(lengd)
+print(y)
 '''
 #5
-lis=[]
-def rada(n):
-    for x in n:
-        if x<n:
+y.reverse()
 
-
-print(rada(lis))
-'''
-
-#QuickSort
-t=[5,2,7,8,4]
-def e(t,l,h):
-    p=t[h]
-    i=l-1
+def b(t,l,h):
+    i =l-1
     for x in range(l,h):
-        if t[x]<=p:
+        if t[x] <= t[h]:
             i+=1
-            tmp=t[x]
-            t[x]=t[i]
-            t[i]=tmp
-    tmp=t[h]
-    t[h]=t[i+1]
-    t[i+1]=tmp
+            t[x],t[i]=t[i],t[x]
+    t[i+1],t[h]=t[h],t[i+1]
     return i+1
-
-def q(t,l,h):
+    
+def p(t,l,h):
     if l<h:
-        o=e(t,l,h)
-        q(t,l,o-1)
-        q(t,o+1,h)
-
-q(t,0,len(t)-1)
-print(t)
+        k=b(t,l,h)
+        p(t,l,k-1)
+        p(t,k+1,h)
+p(y,0,len(y)-1)
+print(y)
