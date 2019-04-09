0004178eb266bbac40ae63e36bae0c790b42d765
Kalliarolunni <gustigaur@gmail.com>
2019-04-09 12:29:20 +0000

    Update skilaverkefni6.py

--- skilaverkefni6.py
+++ skilaverkefni6.py
@@ -11,7 +11,7 @@ def flatarmal_falls(fall, upp, down):
     for x in fall:
         if x == "+" or x == "-":
             merki.append(x)
-    fall_listi = re.split('[+-]', fall)git
+    fall_listi = re.split('[+-]', fall)
     try:
         fall_listi.remove("")
     except:
@@ -19,7 +19,7 @@ def flatarmal_falls(fall, upp, down):
     buid = []
     for x in fall_listi:
         if "x" in x:
-            a= x.index("x")
+            a=x.index("x")
             temp_listi1 = x[:a]
             temp_listi2 = x[a + 1:]
             if a == 0 and len(x) == 2:
@@ -50,22 +50,24 @@ def flatarmal_falls(fall, upp, down):
     if buid2[0] == "+":
         buid2 = buid2[1:]
     nytt_fall_a = buid_fall.replace("x", str(down))
-    nytt_fall_b = buid_fall.replace("x",
-
-fall = input("Sláðu inn fall f(x): ")
-fall1 = input("Sláðu inn fall g(x): ")
-upp = float(input("X efri:: "))
-down = float(input("X neðri: ")) str(upp))
+    nytt_fall_b = buid_fall.replace("x", str(upp))
     flatarmal = round(abs(eval(nytt_fall_b) - eval(nytt_fall_a)), 5)
     return flatarmal
 
-flat = flatarmal_falls(fall, upp, down)
+fall2 = input("Sláðu inn fall f(x): ")
+fall1 = input("Sláðu inn fall g(x): ")
+upp = float(input("X efri:: "))
+down = float(input("X neðri: "))
+
+
+flat = flatarmal_falls(fall2, upp, down)
 flat2 = flatarmal_falls(fall1, upp, down)
 print("Flatarmálið milli f(x) og g(x) er", abs(round(flat - flat2, 5)))
 
 
 
 #2 Memoization
+
 with open("triangle.txt", "r") as f:
     a = f.read()
     l = a.split("\n")
@@ -74,26 +76,25 @@ for x in range(len(l)):
     l[x] = l[x].split(" ")
     for i in range(len(l[x])):
         l[x][i] = int(l[x][i])
-
 cache = {}
 
 def maxPath(l, lina, index):
-    summa = l[lina][index]
-    key = str(index) + "," + str(lina)
+    summa=l[lina][index]
+    key=str(index)+","+str(lina)
     if key in cache:
         return list(cache[key].keys())[0]
-    if len(l) == int(lina) + 2:
-        gildi = summa + max(l[lina + 1][index + 1], l[lina + 1][index])
-        cache[key] = {gildi: str(summa) + "," + str(max(l[lina + 1][index + 1], l[lina + 1][index]))}
+    if len(l)==int(lina) + 2:
+        gildi=summa+max(l[lina+1][index+1],l[lina+1][index])
+        cache[key] = {gildi: str(summa)+","+str(max(l[lina + 1][index + 1], l[lina + 1][index]))}
         return gildi
     else:
         path1 = maxPath(l, lina + 1, index)
         path2 = maxPath(l, lina + 1, index + 1)
         gildi = summa + max(path1, path2)
         if path1 > path2:
-            cache[key] = {gildi: str(l[lina][index]) + "," + cache[str(index) + "," + str(lina + 1)][path1]}
-        elif path1 <= path2:
-            cache[key] = {gildi: str(l[lina][index]) + "," + cache[str(index + 1) + "," + str(lina + 1)][path2]}
+            cache[key]={gildi: str(l[lina][index])+","+cache[str(index)+","+str(lina+1)][path1]}
+        elif path1<=path2:
+            cache[key]={gildi: str(l[lina][index])+","+cache[str(index + 1)+","+str(lina+1)][path2]}
 
         return gildi
 
@@ -136,15 +137,15 @@ class Node:
     def preOrderPrintN(self):
         print(self.value)
         if self.left:
-            self.left.preOrderPrint()
+            self.left.preOrderPrintN()
         if self.right:
-            self.right.preOrderPrint()
+            self.right.preOrderPrintN()
 
     def postOrderPrintN(self):
         if self.left:
-            self.left.postOrderPrint()
+            self.left.postOrderPrintN()
         if self.right:
-            self.right.postOrderPrint()
+            self.right.postOrderPrintN()
         print(self.value)
 
 
@@ -160,10 +161,10 @@ class Tree:
             return True
     def preOrderPrint(self):
         if self.root:
-           return self.root.preOrderPrint()
+           return self.root.preOrderPrintN()
     def postOrderPrint(self):
         if self.root:
-           return self.root.postOrderPrint()
+           return self.root.postOrderPrintN()
     def delete(self):
         if self.root:
            return self.root.delete()
@@ -173,13 +174,15 @@ class Tree:
         # Þinn kóði hér
 
 t = Tree()
-t.insert(27)
-t.insert(7)
-t.insert(57)
-t.insert(62)
-t.insert(13)
-t.insert(56)
+t.insert(4)
+t.insert(3)
 t.insert(2)
-t.preOrderPrintN()
-t.postOrderPrintN()
+t.insert(8)
+t.insert(6)
+t.insert(10)
+t.insert(20)
+print("pre")
+t.preOrderPrint()
+print("post")
+t.postOrderPrint()
 
